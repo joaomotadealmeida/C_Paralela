c6b05edff15cb1808cca8efe4f09d5e9a5cac7e2
Laura Abi Daud <142590244+aaaaabi@users.noreply.github.com>
2023-11-23 20:25:48 -0300

    Update projeto_sequencial.c
    ta funcionando certinho agora (tava com uma variavel de resultado parcial no lugar da variavel resultado

--- grupo_top/projeto/projeto_sequencial.c
+++ grupo_top/projeto/projeto_sequencial.c
@@ -4,11 +4,11 @@
 // FUNCAO FATORIAL
 void funcfatorial(int n, mpz_t resultado) {
     if (n == 0) {
-        mpz_set_ui(resultado, 1);  // Se n = 0, fatorial = 1
+        mpz_set_ui(resultado, 1);         // Se n = 0, fatorial = 1
     } else {
-        mpz_t temp; // MPZ representa funções aritméticas inteiras de alto nível de precisão 
+        mpz_t temp;                       // MPZ representa funções aritméticas inteiras de alto nível de precisão 
         mpz_init(temp);                   // Inicializa uma variável temporária
-        funcfatorial(n - 1, temp);            // Chama recursivamente para calcular (n-1)!
+        funcfatorial(n - 1, temp);        // Chama recursivamente para calcular (n-1)!
         mpz_mul_ui(resultado, temp, n);   // Multiplica o resultado por n
         mpz_clear(temp);                  // Libera a variável temporária
     }
@@ -16,12 +16,12 @@ void funcfatorial(int n, mpz_t resultado) {
 
 // FUNCAO TESTE DE EULER
 void EulerTest(int precisao, mpf_t resultado) {
-  mpf_t resultado_parcial;  // MPF representa funções aritméticas de ponto flutuante de alto nível de precisão 
-  mpf_init2(resultado_parcial, 8192); // Inicializa a variável para resultado parcial
+  mpf_t resultado_parcial;                // MPF representa funções aritméticas de ponto flutuante de alto nível de precisão 
+  mpf_init2(resultado_parcial, 10000);    // Inicializa a variável para resultado parcial
   mpz_t fatorial;
-  mpz_init(fatorial); // Inicializa a variável armazenando fatoriais
+  mpz_init(fatorial);                     // Inicializa a variável armazenando fatoriais
 
-  mpf_init2(fatorial, 8192); // Inicializa o resultado final
+  mpf_init2(resultado, 10000);             // Inicializa o resultado final
 
   // Loop
   for (int i = 0; i <= precisao; i++) {
@@ -32,7 +32,7 @@ void EulerTest(int precisao, mpf_t resultado) {
   }
 
   mpf_clear(resultado_parcial); // Libera a variável para resultado parcial
-  mpz_clear(fatorial); // Libera a variável para os fatoriais
+  mpz_clear(fatorial);          // Libera a variável para os fatoriais
 }
 
 int main() {
